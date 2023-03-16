1afc4b0c45c3a9f0e2da2f3215688054134fa250
Paul <paul.chevilley@gmail.com>
2023-03-15 23:03:09 -0400

    update colorscheme

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,4 +1,16 @@
 vim.opt.background = "dark"
 
-vim.g.material_style = "palenight"
-vim.cmd 'colorscheme material'
+
+require('rose-pine').setup({
+	dark_variant = 'moon',
+    highlight_groups = {
+		ColorColumn = { bg = 'rose' },
+
+		-- Blend colours against the "base" background
+		CursorLine = { bg = 'foam', blend = 40 },
+		StatusLine = { fg = 'love', bg = 'love', blend = 40 },
+	}
+})
+
+-- Set colorscheme after options
+vim.cmd('colorscheme rose-pine')
